affc2cefa896f05cca7b35c8c65b1a3d9fdcdb6f
basker19moon <52462387+basker19moon@users.noreply.github.com>
2019-07-25 01:44:58 +0530

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,6 @@
+a = ( input('Enter your Name : '))
+b = int(input('Enter your Age : '))
+e =int( input('Enter any number you want to Print message that many times : '))
+c = 2019-b
+d= c +100
+print( e* ('\nHey ' + a + ' You will become 100 Years Old By ' + str(d)))
